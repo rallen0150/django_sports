9ca22728a69eea86e4c52584c3b158ac248f07fa
rallen0150 <rallen0150@gmail.com>
2016-10-04 14:25:21 -0400

    created a database through Django

---
+++ sports/my_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Roster(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    position = models.CharField(max_length=2)
+    points = models.FloatField()
+    rebounds = models.FloatField()
+    assists = models.FloatField()
+    steals = models.FloatField()
+    blocks = models.FloatField()
+    college = models.CharField(max_length=10)
